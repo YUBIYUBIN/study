29915003e2fb57a7bd723dab5dbf852fbdcff54d
YUBIN <yubin8938@gmail.com>
2026-01-28 14:20:26 +0900

    Move orders page to React

--- REACT_COURSE/ecommerce-project/src/App.jsx
+++ REACT_COURSE/ecommerce-project/src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router";
 import "./App.css";
 import { HomePage } from "./Pages/HomePage";
 import { CheckoutPage } from "./Pages/CheckoutPage";
+import { OrdersPage } from "./Pages/OrdersPage";
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
     </Routes>
   );
 }
